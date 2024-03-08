243f1f05c7f4ba312de0102bba42302636dab05f
Lucas Garcia <32649703+Garcluca@users.noreply.github.com>
2024-03-08 12:33:46 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import { glslify } from "vite-plugin-glslify";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), glslify()],
-  base:'/PortfolioSite/',
+  base:'https://garcluca.github.io/PortfolioSite/',
   assetsInclude: ["**/*.glb"],
 });
